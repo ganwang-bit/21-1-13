0d69224fe5174b359c20c69c9cc8832c2cb0a39d
ganwang <74584251+ganwang-bit@users.noreply.github.com>
2021-01-13 19:37:54 +0800

    21-1-13

---
+++ 21-1-12.2/main.cpp
@@ -0,0 +1,43 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "Hello world!" << endl;
+//    return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//
+////int main()
+////{
+////    int i,t=1,s=0;
+////    for(i=1;i<=101;i+=2)
+////    {
+////        t*=i;
+////        s+=t;
+////        printf("%d ",t);
+////        t=t/i*-1;
+////    }
+////    printf("\n%d",s);
+////    return 0;
+////}
+//int main()
+//{
+//    int number;
+//    cin>>number;
+//    cout<<"The number is "<<number<<endl;
+//    return 0;
+//}
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<a-b<<' '<<b<<' '<<a<<endl;
+    return 0;
+}
